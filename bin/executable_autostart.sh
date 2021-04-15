383c77d5ddeb3d9ade7058d05ffb83475df32f49
Steven Van Acker <steven@singularity.be>
2021-04-15 10:59:17 +0200

    PATH may not be set. also keep going if there are errors for some files

--- bin/executable_autostart.sh
+++ bin/executable_autostart.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
 
 # This runs each time we login
+export PATH=~/bin:$PATH
 
 # login to lastpass
 lpass login lastpass@singularity.be
 
 # sync chezmoi
-chezmoi update
+chezmoi update --apply --keep-going
 
 # init repos
 ~/bin/init-repos.sh
