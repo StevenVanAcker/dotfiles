1f76a92ef5ef3dabb72ff85684a9acb29ada5ede
Steven Van Acker <steven@singularity.be>
2021-09-02 16:38:11 +0200

    updates

--- bin/executable_autostart.sh
+++ bin/executable_autostart.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
 
 # This runs each time we login
+(
 ~/bin/init-dotfiles.sh
 ~/bin/init-workstation.sh
 ~/bin/init-repos.sh
 
 # cleanup after installer
 rm -f ~/on-install.sh
+) &> ~/.log.autostart
